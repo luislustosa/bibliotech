6d30ae14d0467b0622f885aef6796f69fadc74ae
luislustosa <165705506+luislustosa@users.noreply.github.com>
2025-11-14 09:04:48 -0300

    Initialize Django settings with basic configuration

---
+++ settings.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    # Nossos Apps
+    'catalog.apps.CatalogConfig',
+    'api.apps.ApiConfig',
+]
+
+MIDDLEWARE = [
+    
+]
+
+ROOT_URLCONF = 'bibliotech.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'], # Configurado
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'bibliotech.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
